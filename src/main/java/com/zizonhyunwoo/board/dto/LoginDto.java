f8f0af456540c07b0c6712941c63e7f794e122f9
hyunwoo <daersh@naver.com>
2026-03-02 21:58:29 +0900

    fix: 로그인 dto 비밀번호 로그에 남지 않도록 tostring에서 제외

--- src/main/java/com/zizonhyunwoo/board/dto/LoginDto.java
+++ src/main/java/com/zizonhyunwoo/board/dto/LoginDto.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Getter@NoArgsConstructor@AllArgsConstructor@ToString
+@Getter@NoArgsConstructor@AllArgsConstructor
+@ToString(exclude = "password")
 public class LoginDto {
     @NotBlank
     private String email;
